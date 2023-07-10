ab10162d4a4f5e877b7a2e3d2e46132284261fab
Stoginator <rastogitanish@ymail.com>
2023-07-10 11:31:49 -0400

    Removed push js, updated avatar and indiana to latest review template

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 function controlNav() {
   if (document.getElementById("sidebar").className == "sidebar-hidden") {
     document.getElementById("sidebar").className = "sidebar";
-    document.getElementById("main").className = "push";
   } else {
     document.getElementById("sidebar").className = "sidebar-hidden";
-    document.getElementById("main").className = "no-push";
   }
 }
