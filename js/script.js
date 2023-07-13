9b207315215d48d256c17fccab4d172879ca79b8
Stoginator <rastogitanish@ymail.com>
2023-07-13 10:24:38 -0400

    Avoid FOUC, site is completely working now

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Sidebar logic
+// Sidebar logic, called by hamburger icon
 function controlNav() {
   if (document.getElementById("sidebar").className == "sidebar-hidden") {
     document.getElementById("sidebar").className = "sidebar";
@@ -7,29 +7,34 @@ function controlNav() {
   }
 }
 
-// Toggle dark and light modes
+// Toggle dark and light modes, called by theme-btn
 function themeToggle() {
   const btn = document.getElementById("toggle-btn");
-  if (document.body.className == "light-theme") {
-    document.body.className = "";
+  if (document.firstElementChild.getAttribute("data-theme") == "light") {
+    document.firstElementChild.setAttribute("data-theme", "dark");
     btn.innerText = "Toggle Light Mode";
     localStorage.setItem("theme", "dark");
   } else {
-    document.body.className = "light-theme";
-    localStorage.setItem("theme", "light");
+    document.firstElementChild.setAttribute("data-theme", "light");
     btn.innerText = "Toggle Dark Mode";
+    localStorage.setItem("theme", "light");
   }
 }
 
-// Store user preference for light and dark mode across visits in same browser
-window.addEventListener("load", (event) => {
-  const btn = document.getElementById("toggle-btn");
-  const currentTheme = localStorage.getItem("theme");
-  if (currentTheme == "light") {
-    document.body.className = "light-theme";
-    btn.innerText = "Toggle Dark Mode";
-  } else if (currentTheme == "dark") {
-    document.body.className = "";
-    btn.innerText = "Toggle Light Mode";
+// Logic for maintaining OS color theme and overriding theme
+// Avoid FOUC by maintaining data-theme attribute on root HTML element
+const modeStorageKey = "theme";
+
+const getTheme = () => {
+  if (localStorage.getItem(modeStorageKey)) {
+    return localStorage.getItem(modeStorageKey);
+  } else {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
   }
-});
+}
+
+const setTheme = () => {
+  document.firstElementChild.setAttribute("data-theme", getTheme());
+}
+
+setTheme();
